68b58072695dc3654a96fe760b482d3bc3604fcc
Arthur Protti Ramos <148826914+Protti22@users.noreply.github.com>
2024-11-26 21:56:59 -0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -6,9 +6,16 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('owners', [CarController::class, 'index']);
-Route::get('owners/create', [CarController::class, 'create']);
-Route::post('owners', [CarController::class, 'store']);
-Route::get('owners/{id}/edit', [CarController::class, 'edit']);
-Route::put('owners/{id}', [CarController::class, 'update']);
-Route::delete('owners/{id}', [CarController::class, 'destroy']);
+Route::get('cars', [CarController::class, 'index']);
+Route::get('cars/create', [CarController::class, 'create']);
+Route::post('cars', [CarController::class, 'store']);
+Route::get('cars/{id}/edit', [CarController::class, 'edit']);
+Route::put('cars/{id}', [CarController::class, 'update']);
+Route::delete('cars/{id}', [CarController::class, 'destroy']);
+
+Route::get('owners', [OwnerController::class, 'index']);
+Route::get('owners/create', [OwnerController::class, 'create']);
+Route::post('owners', [OwnerController::class, 'store']);
+Route::get('owners/{id}/edit', [OwnerController::class, 'edit']);
+Route::put('owners/{id}', [OwnerController::class, 'update']);
+Route::delete('owners/{id}', [OwnerController::class, 'destroy']);
